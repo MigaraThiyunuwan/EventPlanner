9d528fd6bb189479fb6f1e6857f961acac5ed4db
MigaraThiyunuwan <86980643+MigaraThiyunuwan@users.noreply.github.com>
2023-12-15 16:21:59 +0530

    edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 
 // import './App.css';
 // import AdminDashBoard from './components/AdminDashBoard';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './css/style.css';
+import Home from './pages/Home'
 import AdminDashboard from './pages/AdminDashboard';
+import AdminProfile from './pages/AdminProfile';
 
 // import HelloWorld from './HelloWorld';
 
@@ -13,8 +16,14 @@ function App() {
   return (
     // <HelloWorld/>
    
-    <AdminDashboard/>
-
+    
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="admindashbord" element={<AdminDashboard/>}></Route>
+      <Route path="adminprofile" element={<AdminProfile/>}></Route>
+    </Routes>
+    </BrowserRouter>
   
   );
 }
---
+++ frontend/src/pages/AdminProfile.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavigationBar from '../components/NavigationBar'
+import '../css/adminProfile.css'
+
+function AdminProfile() {
+  return (
+    <div>
+      <NavigationBar/>
+
+
+        
+    </div>
+  )
+}
+
+export default AdminProfile
