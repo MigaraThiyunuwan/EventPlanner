d0dc2e2cbf8392b5ea3aa1de44d174a7fb5ff6a7
NimashaWeragama <cst20095@std.uwu.ac.lk>
2024-01-25 14:34:08 +0530

    create product and order apis

--- backend/event_planner_api/models.py
+++ backend/event_planner_api/models.py
@@ -13,3 +13,28 @@ class Customer(models.Model):
 
     class Meta:
         db_table = "customer"
+
+class ProductType(models.TextChoices):
+    PHOTOGRAPHY = "PH", "Photography"
+    CATERING = "CA", "Catering"
+    DECORATION = "DE", "Decoration"
+    MUSIC = "MU", "Music"
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    productType = models.CharField(
+        max_length=2,
+        choices=ProductType.choices,
+        default=ProductType.PHOTOGRAPHY,
+    )
+
+class Service(models.Model):
+    product = models.ForeignKey(Product, related_name='services', on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    customer = models.ForeignKey(Customer, related_name='orders', on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, related_name='orders', blank=True)
--- backend/event_planner_api/urls.py
+++ backend/event_planner_api/urls.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
 from rest_framework_simplejwt import views as jwt_views
+from .products.views import ProductViewSet
+from .orders.views import OrderViewSet
+
+router = routers.DefaultRouter()
+router.register(r'product', ProductViewSet, basename="product")
+router.register(r'order', OrderViewSet, basename="order")
 
 urlpatterns = [
     path('hello-world/', views.hello_world, name='hello_world'),
 
-    path('validate_signup', views.validate_signup, name='validate_signup')
+    path('validate_signup', views.validate_signup, name='validate_signup'),
+
+    path('', include(router.urls)),
     
 ]
